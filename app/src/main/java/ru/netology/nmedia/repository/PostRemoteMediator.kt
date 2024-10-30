a0817135d6c24e1af71a9c920b93d8fdc7b1d62f
shefyo <olegshefner6@gmail.com>
2024-10-30 19:28:56 +0300

    Arch3

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRemoteMediator.kt
@@ -0,0 +1,68 @@
+package ru.netology.nmedia.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import ru.netology.nmedia.api.ApiService
+import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dao.PostRemoteKeyDao
+import ru.netology.nmedia.db.AppDb
+import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.PostRemoteKeyEntity
+import ru.netology.nmedia.entity.toEntity
+import ru.netology.nmedia.error.ApiError
+
+@OptIn(ExperimentalPagingApi::class)
+class PostRemoteMediator(
+    private val service: ApiService,
+    private val db: AppDb,
+    private val postDao: PostDao,
+    private val postRemoteKeyDao: PostRemoteKeyDao,
+) : RemoteMediator<Int, PostEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, PostEntity>
+    ): MediatorResult {
+        try {
+            val response = when (loadType) {
+                LoadType.REFRESH -> {
+                    service.getLatest(state.config.initialLoadSize)
+                }
+                LoadType.APPEND -> {
+                    val id = postRemoteKeyDao.min() ?: return MediatorResult.Success(
+                        endOfPaginationReached = false
+                    )
+                    service.getBefore(id, state.config.pageSize)
+                }
+                else -> return MediatorResult.Success(endOfPaginationReached = false)
+            }
+
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(
+                response.code(),
+                response.message(),
+            )
+
+            db.withTransaction {
+                when (loadType) {
+                    LoadType.REFRESH -> {
+                        postDao.insert(body.toEntity())
+                    }
+                    LoadType.APPEND -> {
+                        postDao.insert(body.toEntity())
+                    }
+                    LoadType.PREPEND -> {
+
+                    }
+                }
+            }
+            return MediatorResult.Success(endOfPaginationReached = body.isEmpty())
+        } catch (e: Exception) {
+            return MediatorResult.Error(e)
+        }
+    }
+}
